37c1f8e7a70ca7638f84a7a06ac24af90066194d
Giancarlo <giianbos@gmail.com>
2023-08-10 10:25:34 +0800

    fixed issues

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import {Home} from './pages/Home';
+import { Specialist, Engineer, Pilot, Commander } from './pages/Crew';
+import { Moon, Mars, Europa, Titan } from './pages/Destination';
+import { Capsule, Spaceport, Vehicle } from "./pages/Technology";
+import './styles/styles.css';
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/space-tourism' element={<Home />} />
+          <Route path='mark' element={<Specialist />} />
+          <Route path='douglas' element={<Commander />} />
+          <Route path='anousheh' element={<Engineer />} />
+          <Route path='victor' element={<Pilot />} />
+          <Route path='moon' element={<Moon />} />
+          <Route path='mars' element={<Mars />} />
+          <Route path='europa' element={<Europa />} />
+          <Route path='titan' element={<Titan />} />
+          <Route path='space-capsule' element={<Capsule />} />
+          <Route path='spaceport' element={<Spaceport />} />
+          <Route path='vehicle' element={<Vehicle />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
